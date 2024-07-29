a0c44f733d98c11eee3bb917ea09b360cb9382df
Dax-savani <dakshsavani15@gmail.com>
2024-07-29 10:38:21 +0530

    cors configs updated

--- index.js
+++ index.js
@@ -18,7 +18,10 @@ const orderRouter = require("./routes/order");
 connectionDB(process.env.DB_CONNECTION_STRING);
 
 //Middlewares
-app.use(cors());
+app.use(cors({
+    origin: 'http://localhost:3000', // your frontend's domain
+    credentials: true
+}));
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({extended: false}));
