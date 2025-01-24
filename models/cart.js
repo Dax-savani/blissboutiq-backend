cff0e24c0dca2ab3e1f14a646f28fd4e11240e59
Dax-savani <dakshsavani15@gmail.com>
2025-01-24 16:00:12 +0530

    wishlist completed

--- models/cart.js
+++ models/cart.js
@@ -15,30 +15,10 @@ const cartSchema = new Schema({
     color_id: {
         type: String,
         required: true,
-        validate: {
-            validator: async function (value) {
-                const product = await mongoose.model("Product").findById(this.product_id);
-                if (!product) return false;
-                return product.color_options.some(option => option._id.toString() === value);
-            },
-            message: "Invalid color ID selected for the product."
-        }
     },
     size: {
         type: String,
         required: true,
-        validate: {
-            validator: async function (value) {
-                const product = await mongoose.model("Product").findById(this.product_id);
-                if (!product) return false;
-
-                const selectedColor = product.color_options.find(option => option._id.toString() === this.color_id);
-                if (!selectedColor) return false;
-
-                return selectedColor.size_options.some(option => option.size === value);
-            },
-            message: "Invalid size selected for the product."
-        }
     },
     qty: {
         type: Number,
