e49c36002f4e3567f644079da3b74f557275b911
Dax-savani <dakshsavani15@gmail.com>
2025-03-06 10:48:57 +0530

    issue fixed

--- models/user.js
+++ models/user.js
@@ -22,7 +22,6 @@ const userSchema = new mongoose.Schema({
   phone_number: {
     type: String,
     required: true,
-    unique: true,
     trim: true,
     match: [/^\d{10}$/, "Phone number must be 10 digits"],
   },
