71cf9692155ef299d54200c0cddb194aa5703908
Dax-savani <dakshsavani15@gmail.com>
2025-03-06 10:28:42 +0530

    issue fixed

--- models/contact.js
+++ models/contact.js
@@ -5,7 +5,7 @@ const contactSchema = new mongoose.Schema({
     lastName: { type: String, required: true },
     message: { type: String },
     phoneNumber: { type: String, required: true },
-    email: { type: String, required: true, unique: true },
+    email: { type: String, required: true },
 }, { timestamps: true });
 
 const Contact = mongoose.model('Contact', contactSchema);
